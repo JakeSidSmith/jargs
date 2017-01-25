050c89d244dc54516ad2fe4438a92fcdf2c1df77
Jake 'Sid' Smith <jake@dabapps.com>
2017-01-25 12:11:09 +0000

    Remove collect comments

--- src/collect.js
+++ src/collect.js
@@ -17,15 +17,10 @@
 
   function collect (/* program, command, argv, ...tree */) {
     var args = argsToArray(arguments);
-    var program = args.shift();
-    var command = args.shift();
+    /* var program = */ args.shift();
+    /* var command = */ args.shift();
     var argv = args.shift();
     var tree = args;
-
-    console.log(program);
-    console.log(command);
-    console.log(argv);
-    console.log(tree);
 
     return new Jarg(argv, tree, 0);
   }
