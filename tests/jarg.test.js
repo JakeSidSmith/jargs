5b6150162c7526d455b139c528f5ac48c1758267
Jake 'Sid' Smith <jake@dabapps.com>
2017-01-25 12:25:18 +0000

    Test constructing a Jarg instance

--- tests/jarg.test.js
+++ tests/jarg.test.js
@@ -7,12 +7,31 @@
   var expect = require('chai').expect;
 
   var Jarg = require('../src/jarg');
+  var Command = require('../src/command');
+  var Arg = require('../src/arg');
 
   describe('jarg.js', function () {
 
     it('should exist', function () {
       expect(Jarg).to.be.ok;
       expect(typeof Jarg).to.equal('function');
+    });
+
+    it('should construct a Jarg instance', function () {
+      var argv = ['install', 'jargs', '--save'];
+
+      var tree = [Command('install', null, Arg('lib'))];
+
+      var result = new Jarg(argv, tree);
+
+      expect(result._argv).to.equal(argv);
+      expect(result._tree).to.equal(tree);
+      expect(result._depth).to.equal(0);
+
+      expect(result._commands).to.eql({install: tree[0]});
+      expect(result._kwargs).to.eql({});
+      expect(result._flags).to.eql({});
+      expect(result._args).to.eql({});
     });
 
   });
