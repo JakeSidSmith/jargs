b611d8443fce22e168572f2a8f8619bdd30dec19
Jake 'Sid' Smith <jake@dabapps.com>
2017-01-25 10:47:28 +0000

    Improve duplicate node / add node

--- src/jarg.js
+++ src/jarg.js
@@ -3,45 +3,39 @@
 (function () {
 
   function Jarg (argv, tree, depth) {
-    this._argv = argv;
-    this._tree = tree;
-    this._depth = depth;
+    var self = this;
 
-    this._commands = {};
-    this._kwargs = {};
-    this._flags = {};
-    this._args = {};
+    self._argv = argv;
+    self._tree = tree;
+    self._depth = depth;
 
-    for (var i = 0; i < this._tree.length; i += 1) {
-      var node = this._tree[i];
+    self._commands = {};
+    self._kwargs = {};
+    self._flags = {};
+    self._args = {};
+
+    function addNode (node) {
+      var type = node._type;
+
+      if (node.name in self['_' + type + 's']) {
+        throw new Error('Duplicate ' + type + ' \'' + node.name + '\' in tree at depth ' + self._depth);
+      }
+
+      self['_' + type + 's'][node.name] = node;
+    }
+
+    for (var i = 0; i < self._tree.length; i += 1) {
+      var node = self._tree[i];
 
       switch (node._type) {
         case 'command':
-          if (node.name in this._commands) {
-            throw new Error('Duplicate command \'' + node.name + '\' in tree at depth ' + this._depth);
-          }
-          this._commands[node.name] = node;
-          break;
         case 'kwarg':
-          if (node.name in this._kwargs) {
-            throw new Error('Duplicate kwarg \'' + node.name + '\' in tree at depth ' + this._depth);
-          }
-          this._kwargs[node.name] = node;
-          break;
         case 'flag':
-          if (node.name in this._flags) {
-            throw new Error('Duplicate flag \'' + node.name + '\' in tree at depth ' + this._depth);
-          }
-          this._flags[node.name] = node;
-          break;
         case 'arg':
-          if (node.name in this._args) {
-            throw new Error('Duplicate arg \'' + node.name + '\' in tree at depth ' + this._depth);
-          }
-          this._args[node.name] = node;
+          addNode(node);
           break;
         default:
-          throw new Error('Unknown node type \'' + node._type + '\'in tree at depth ' + this._depth);
+          throw new Error('Unknown node type \'' + node._type + '\'in tree at depth ' + self._depth);
       }
     }
   }
