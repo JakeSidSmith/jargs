050359c7399b9e8905d114a2438fa7e374da9811
Jake 'Sid' Smith <jake@dabapps.com>
2017-01-25 10:06:25 +0000

    Test that collect returns a Jarg instance

--- tests/collect.test.js
+++ tests/collect.test.js
@@ -5,12 +5,26 @@
   var expect = require('chai').expect;
 
   var collect = require('../src/collect');
+  var Jarg = require('../src/jarg');
+  var Command = require('../src/command');
 
   describe('collect.js', function () {
 
     it('should exist', function () {
       expect(collect).to.be.ok;
       expect(typeof collect).to.equal('function');
+    });
+
+    it('should return a Jarg instance', function () {
+      var boundCollect = collect.bind(null, 'node', 'npm', ['install', 'jargs', '--save']);
+
+      var result = boundCollect(
+        Command(
+          'npm'
+        )
+      );
+
+      expect(result instanceof Jarg).to.be.true;
     });
 
   });
