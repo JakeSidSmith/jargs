e49e99b6382cc5258f650f7db84054232b3d9d6f
Jake 'Sid' Smith <jake@dabapps.com>
2017-01-29 02:07:05 +0000

    Update Arg test

--- tests/arg.test.js
+++ tests/arg.test.js
@@ -11,11 +11,11 @@
   describe('arg.js', function () {
 
     it('should construct an Arg', function () {
-      var node = Arg('foo', {alias: 'bar'}, 'child1', 'child2');
+      var node = Arg('foo', null, 'child1', 'child2');
 
       expect(node).to.be.ok;
       expect(node.name).to.equal('foo');
-      expect(node.options).to.eql({alias: 'bar'});
+      expect(node.options).to.eql({});
       expect(node.children).to.eql(['child1', 'child2']);
       expect(node._type).to.equal('arg');
     });
