b25ffb672dd405d543b3d7761cdfd11f71e17fe0
Jake 'Sid' Smith <jake@dabapps.com>
2017-02-01 02:56:39 +0000

    Update KWArg, Flag, and Arg tests

--- tests/flag.test.js
+++ tests/flag.test.js
@@ -11,13 +11,19 @@
   describe('flag.js', function () {
 
     it('should construct a Flag', function () {
-      var node = Flag('foo', {alias: 'bar'}, 'child1', 'child2');
+      var node = Flag('foo', {alias: 'bar'});
 
       expect(node).to.be.ok;
       expect(node.name).to.equal('foo');
       expect(node.options).to.eql({alias: 'bar'});
       expect(node.children).to.be.undefined;
       expect(node._type).to.equal('flag');
+    });
+
+    it('should throw an error if has children', function () {
+      var anError = /children/i;
+
+      expect(Flag.bind(null, 'foo', null, 'child')).to.throw(anError);
     });
 
   });
--- tests/kwarg.test.js
+++ tests/kwarg.test.js
@@ -11,13 +11,19 @@
   describe('kwarg.js', function () {
 
     it('should construct a KWArg', function () {
-      var node = KWArg('foo', {alias: 'bar'}, 'child1', 'child2');
+      var node = KWArg('foo', {alias: 'bar'});
 
       expect(node).to.be.ok;
       expect(node.name).to.equal('foo');
       expect(node.options).to.eql({alias: 'bar'});
       expect(node.children).to.be.undefined;
       expect(node._type).to.equal('kwarg');
+    });
+
+    it('should throw an error if has children', function () {
+      var anError = /children/i;
+
+      expect(KWArg.bind(null, 'foo', null, 'child')).to.throw(anError);
     });
 
   });
