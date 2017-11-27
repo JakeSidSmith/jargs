d2606943b9a6759f76b70da8090393971cd49324
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-27 14:08:51 +0000

    Update tests

--- tests/kwarg.test.js
+++ tests/kwarg.test.js
@@ -15,7 +15,7 @@
 
       expect(node).to.be.ok;
       expect(node.name).to.equal('foo');
-      expect(node.options).to.eql({alias: 'f', description: ''});
+      expect(node.options).to.eql({alias: 'f', description: '', multi: false});
       expect(node.children).to.be.undefined;
       expect(node._type).to.equal('kwarg');
     });
