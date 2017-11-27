09872ea83a24e28a79b1da6b5cb4947301f927af
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-27 15:46:36 +0000

    Prepare types for rest node

--- index.d.ts
+++ index.d.ts
@@ -18,6 +18,7 @@ declare module 'jargs' {
     kwargs: ArgsOrKWArgs;
     flags: Flags;
     args: ArgsOrKWArgs;
+    rest: undefined | Array<string | undefined>;
   }
 
   export interface HelpProps {
