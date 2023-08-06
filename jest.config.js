b12529c55f7e20c6c9d90f812d712a03d1f6cc67
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-08-06 22:10:49 +0100

    Fix jest config to work with js files

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-babel-esm',
   collectCoverageFrom: ['src/**/*.(js|jsx|ts|tsx)'],
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 100,
-  //     functions: 100,
-  //     lines: 100,
-  //     statements: 100,
-  //   },
-  // },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   testRegex: 'tests/.+\\.(ts|tsx|js|jsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testEnvironmentOptions: {
